73dbac054e122804fdf7b6f4a87d1af8fd289e7a
Haochen M. Kotoi-Xie <haochenx@acm.org>
2023-11-18 08:54:12 +0900

    mobile-app - android setup ok

---
+++ jsland/apps/mobile-app/metro.config.js
@@ -0,0 +1,25 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require("path");
+
+const workspaceRoot = path.resolve(__dirname, "../..");
+const projectRoot = __dirname;
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [workspaceRoot];
+// 2. Let Metro know where to resolve packages, and in what order
+config.resolver.nodeModulesPaths = [
+  // path.resolve(projectRoot, "melange/output/node_modules"),
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+];
+// 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
+config.resolver.disableHierarchicalLookup = true;
+
+module.exports = config;
